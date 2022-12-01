726beaddadd1b92ad7d8111788e98abe51ad696f
JulianZea <cesarjulianzeabravo@gmail.com>
2022-11-30 21:18:53 -0500

    updated file hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application:"""
+""" Starting a Flask web application """
 from flask import Flask
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def hello_world():
-    """ method that return mesages in the server"""
-    return "Hello HBNB!"
+@app.route('/')
+def hello_hbnb():
+    """ display a message """
+    return 'Hello HBNB!'
 
 
-if (__name__ == '__main__'):
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
