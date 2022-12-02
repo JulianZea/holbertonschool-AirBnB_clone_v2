49da8cc4605acc2213eff77ae967d7ea2acecec7
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-02 13:20:31 -0500

    fix file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -24,16 +24,14 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
-    c_fun = text.replace('_', ' ')
-    return 'C {}'.format(c_fun)
+    return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
-    c_fun = text.replace('_', ' ')
-    return 'C {}'.format(c_fun)
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
@@ -44,8 +42,9 @@ def imanumber(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """Display the int on a h1 tag"""
-    return render_template('5-number.html', key_name=int(n))
+    """ display number int """
+    return render_template('5-number.html', valKeyName=int(n))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
