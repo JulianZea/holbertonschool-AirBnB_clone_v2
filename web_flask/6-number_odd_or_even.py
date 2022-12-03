ce5a1132fe31f93d45a5a164904abb8e6373a703
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-02 21:10:37 -0500

    6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,8 +49,8 @@ def number_template(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def func_odd_even(n):
     """ Only enter the module if n is an odd or even number """
-    number_odd_even = "even" if (n % 2 == 0) else "odd"
-    return render_template("6-number_odd_or_even.html", n=n, number_odd_even=number_odd_even)
+    num_odd_even = "even" if (n % 2 == 0) else "odd"
+    return render_template("6-number_odd_or_even.html", n=n, num_odd_even=num_odd_even)
 
 
 if __name__ == '__main__':
