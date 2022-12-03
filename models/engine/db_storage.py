faebdbcc5cac4dce35bcc208e0d9c908f35c7632
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-02 22:55:54 -0500

    fix db storage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -35,6 +35,14 @@ class DBStorage():
                                              os.getenv("HBNB_MYSQL_HOST"),
                                              os.getenv("HBNB_MYSQL_DB")),
                                       pool_pre_ping=True)
+        self.__my_list = {
+                "User": User,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Place": Place,
+                "Reivew": Review
+                }
 
         if os.getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
@@ -43,9 +51,11 @@ class DBStorage():
         """ Query on current database for all o specify class"""
         dic = {}
         types_obj = [State, City, User, Place, Amenity, Review]
-        if type(cls) == str:
-            return dic
-        if cls is not None and cls in types_obj:
+        # if type(cls) == str:
+        #    return dic
+        if cls is not None and (cls in types_obj or type(cls) == str):
+            if type(cls) == str:
+                cls = self.__my_list[cls]
             query_list = self.__session.query(cls).all()
             for el in query_list:
                 key = "{}.{}".format(type(el).__name__, el.id)
@@ -54,8 +64,8 @@ class DBStorage():
             for typ in types_obj:
                 query_list2 = self.__session.query(typ)
                 for el in query_list2:
-                     key = "{}.{}".format(type(el).__name__, el.id)
-                     dic[key] = el
+                    key = "{}.{}".format(type(el).__name__, el.id)
+                    dic[key] = el
         return dic
 
     def new(self, obj):
